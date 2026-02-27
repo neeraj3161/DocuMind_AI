74eea81ae7f325f71f52b2ecf6edac9877bee4d3
neeraj3161 <neerajtripathi7447@gmail.com>
2026-02-27 15:28:25 +0530

    DocuMindAi: feat: production-ready hybrid RAG backend with pgvector
    Completed implementation of batching in embedding_service.py

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -14,5 +14,5 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def create_embedding(text: str):
-    embedding = model.encode(text, normalize_embeddings=True)
+    embedding = model.encode(text,batch_size=32, normalize_embeddings=True)
     return embedding.tolist()
