f77b551864b7d522733d2e2c1e620cd2083254c0
neeraj3161 <neerajtripathi7447@gmail.com>
2026-03-01 22:09:05 +0530

    DocuMindAi: V-1.0 Added fallback for pdf failure with a new library PDFPlumber

--- app/services/pdf_loader.py
+++ app/services/pdf_loader.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import pdfplumber
 from pypdf import PdfReader
 
 def extract_text_from_pdf(file_path: str):
@@ -8,7 +9,7 @@ def extract_text_from_pdf(file_path: str):
     length_per_page = 0
     for page in reader.pages:
         try:
-            page_text = page.extract_text()
+            page_text = page.get_object().extract_text()
             if page_text:
                 text += page_text + "\n"
                 if length_per_page == 0:
@@ -17,4 +18,15 @@ def extract_text_from_pdf(file_path: str):
             print("Skipping problematic page:", e)
             continue
     print('Extracted text length:', len(text))
+    if len(text) == 0:
+        extract_text_from_pdf_with_pdfplumber(file_path)
     return text
+
+def extract_text_from_pdf_with_pdfplumber(file_path: str):
+    try:
+        # created a fallback if something fails
+        print('Extracting text from PDF plumber')
+        with pdfplumber.open(file_path) as pdf:
+            return "\n".join([page.extract_text() for page in pdf.pages])
+    except Exception as e:
+        print("Skipping problematic page:", e)
